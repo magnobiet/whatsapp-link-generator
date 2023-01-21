679cb3dcd4c7974217cdb2c09bec6cc189513edb
Magno Fernando Brixner Biét <magno.biet@gmail.com>
2023-01-21 18:52:46 -0300

    chore: add ESLint, EditorConfig and Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: [
+    'prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended'
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    eqeqeq: ['error', 'always'],
+    curly: 'error',
+    semi: 'error',
+    'no-unused-vars': 'off',
+    'no-use-before-define': 'error',
+    'react/react-in-jsx-scope': 'off',
+  }
+};
